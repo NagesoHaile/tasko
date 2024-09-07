d02d860540566edf6468db173b9ed1f8df7faa1b
NagesoHaile <nageehail12@gmail.com>
2024-09-07 04:14:56 +0300

    theme setup

--- lib/configs/theme/theme.dart
+++ lib/configs/theme/theme.dart
@@ -1,77 +1,301 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tasko/core/colors/app_colors.dart';
 
-ThemeData lightTheme =
-    ThemeData(useMaterial3: true, colorScheme: lightColorScheme);
+class Style {
+  /// Custom page transitions
+  static const _pageTransitionsTheme = PageTransitionsTheme(
+    builders: {
+      TargetPlatform.android: ZoomPageTransitionsBuilder(),
+      TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+    },
+  );
 
-ThemeData darkTheme =
-    ThemeData(useMaterial3: true, colorScheme: darkColorScheme);
+  /// Light Mode
+  static final ThemeData light = ThemeData(
+    // scaffoldBackgroundColor: AppColors.scafoldBacgroundColor,
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: Colors.black,
+      selectionColor: Colors.black.withOpacity(0.4),
+      selectionHandleColor: Colors.black,
+    ),
+    dropdownMenuTheme: DropdownMenuThemeData(
+      textStyle: GoogleFonts.roboto(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+      ),
+      menuStyle: const MenuStyle(),
+      inputDecorationTheme: InputDecorationTheme(
+        labelStyle: GoogleFonts.roboto(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+        floatingLabelStyle: GoogleFonts.roboto(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+        filled: true,
+        fillColor: Colors.white,
+        border: const OutlineInputBorder(borderSide: BorderSide.none),
+        enabledBorder: const OutlineInputBorder(borderSide: BorderSide.none),
+        focusedBorder: const OutlineInputBorder(borderSide: BorderSide.none),
+      ),
+    ),
+    brightness: Brightness.light,
+    appBarTheme: AppBarTheme(
+      systemOverlayStyle: SystemUiOverlayStyle.light,
+      backgroundColor: AppColors.primaryColor,
+      surfaceTintColor: AppColors.primaryColor,
+      elevation: 0,
+      centerTitle: false,
+      iconTheme: const IconThemeData(color: Colors.white),
+      titleTextStyle: GoogleFonts.roboto(
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+    listTileTheme: ListTileThemeData(
+      iconColor: Colors.black,
+      titleTextStyle: GoogleFonts.roboto(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+      elevation: WidgetStateProperty.all<double>(0),
+      foregroundColor: WidgetStateProperty.resolveWith<Color>(
+        (Set<WidgetState> state) => state.contains(WidgetState.disabled)
+            ? Colors.black54
+            : Colors.black,
+      ),
+      backgroundColor: WidgetStateProperty.resolveWith<Color>(
+        (Set<WidgetState> state) => state.contains(WidgetState.disabled)
+            ? Colors.white54
+            : Colors.white,
+      ),
+      textStyle: WidgetStateProperty.all<TextStyle>(
+        GoogleFonts.inter(
+          fontSize: 15,
+          fontWeight: FontWeight.w500,
+          letterSpacing: 0.6,
+        ),
+      ),
+      shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+        RoundedRectangleBorder(
+          side: const BorderSide(
+            color: Colors.grey,
+            width: 0.3,
+          ),
+          borderRadius: BorderRadius.circular(4.0),
+        ),
+      ),
+    )),
+    tabBarTheme: TabBarTheme(
+      labelStyle: GoogleFonts.roboto(
+        color: Colors.white,
+        fontWeight: FontWeight.w800,
+        fontSize: 14,
+        letterSpacing: 0.8,
+      ),
+      unselectedLabelStyle: GoogleFonts.roboto(
+        fontSize: 14,
+        fontWeight: FontWeight.w800,
+        color: Colors.white70,
+        letterSpacing: 0.8,
+      ),
+      indicatorColor: Colors.white,
+      indicatorSize: TabBarIndicatorSize.tab,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        elevation: WidgetStateProperty.all<double>(0),
+        minimumSize:
+            WidgetStateProperty.all<Size>(const Size(double.infinity, 48)),
+        backgroundColor: WidgetStateProperty.resolveWith<Color>(
+          (Set<WidgetState> state) => state.contains(WidgetState.disabled)
+              ? AppColors.primaryColor
+              : AppColors.primaryColor,
+        ),
+        foregroundColor: WidgetStateProperty.resolveWith<Color>(
+          (Set<WidgetState> state) => state.contains(WidgetState.disabled)
+              ? Colors.white60
+              : Colors.white,
+        ),
+        textStyle: WidgetStateProperty.all<TextStyle>(
+          GoogleFonts.inter(
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            letterSpacing: 0.6,
+          ),
+        ),
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+    ),
+    colorScheme: const ColorScheme.light(),
+    pageTransitionsTheme: _pageTransitionsTheme,
+    textTheme: textTheme.apply(bodyColor: Colors.black),
+    popupMenuTheme: PopupMenuThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(6),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: GoogleFonts.roboto(
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+        color: Colors.black54,
+      ),
+      floatingLabelStyle: GoogleFonts.roboto(
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide(color: Colors.grey.shade500, width: 0.1),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide(color: Colors.grey.shade500, width: 1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: const BorderSide(color: Colors.black, width: 2),
+      ),
+    ),
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      foregroundColor: Colors.white,
+      backgroundColor: AppColors.primaryColor,
+      extendedTextStyle: GoogleFonts.inter(
+        fontSize: 15,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.6,
+      ),
+      // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+    ),
+  );
 
-// Color Schemes
+  /// Dark Mode
+  static final ThemeData dark = ThemeData(
+    textSelectionTheme: TextSelectionThemeData(
+      cursorColor: Colors.white,
+      selectionColor: Colors.white.withOpacity(0.4),
+      selectionHandleColor: Colors.white,
+    ),
+    brightness: Brightness.dark,
+    colorScheme: const ColorScheme.dark(),
+    appBarTheme: AppBarTheme(
+      systemOverlayStyle: SystemUiOverlayStyle.light,
+      backgroundColor: Colors.black,
+      elevation: 0,
+      centerTitle: false,
+      iconTheme: const IconThemeData(color: Colors.white),
+      titleTextStyle: GoogleFonts.roboto(
+        fontSize: 20,
+        fontWeight: FontWeight.w600,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        elevation: WidgetStateProperty.all<double>(0),
+        backgroundColor: WidgetStateProperty.resolveWith<Color>(
+          (Set<WidgetState> state) => state.contains(WidgetState.disabled)
+              ? AppColors.primaryColor
+              : AppColors.primaryColor,
+        ),
+        foregroundColor: WidgetStateProperty.resolveWith<Color>(
+          (Set<WidgetState> state) => state.contains(WidgetState.disabled)
+              ? Colors.white60
+              : Colors.white,
+        ),
+        textStyle: WidgetStateProperty.all<TextStyle>(
+          GoogleFonts.inter(
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+    ),
+    pageTransitionsTheme: _pageTransitionsTheme,
+    textTheme: textTheme.apply(bodyColor: Colors.white),
+    popupMenuTheme: PopupMenuThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(6),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: GoogleFonts.roboto(
+        fontWeight: FontWeight.w400,
+        fontSize: 16,
+        color: Colors.white,
+      ),
+      floatingLabelStyle: GoogleFonts.roboto(
+        fontWeight: FontWeight.w400,
+        color: Colors.white,
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: BorderSide(color: Colors.grey.shade700, width: 0.1),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: const BorderSide(color: Colors.white60, width: 1),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(5),
+        borderSide: const BorderSide(color: Colors.white60, width: 2),
+      ),
+    ),
+  );
+}
 
-const lightColorScheme = ColorScheme(
-  brightness: Brightness.light,
-  primary: Color(0xFF6750A4),
-  onPrimary: Color(0xFFFFFFFF),
-  primaryContainer: Color(0xFFEADDFF),
-  onPrimaryContainer: Color(0xFF21005D),
-  secondary: Color(0xFF625B71),
-  onSecondary: Color(0xFFFFFFFF),
-  secondaryContainer: Color(0xFFE8DEF8),
-  onSecondaryContainer: Color(0xFF1D192B),
-  tertiary: Color(0xFF7D5260),
-  onTertiary: Color(0xFFFFFFFF),
-  tertiaryContainer: Color(0xFFFFD8E4),
-  onTertiaryContainer: Color(0xFF31111D),
-  error: Color(0xFFB3261E),
-  onError: Color(0xFFFFFFFF),
-  errorContainer: Color(0xFFF9DEDC),
-  onErrorContainer: Color(0xFF410E0B),
-  outline: Color(0xFF79747E),
-  background: Color(0xFFFFFBFE),
-  onBackground: Color(0xFF1C1B1F),
-  surface: Color(0xFFFFFBFE),
-  onSurface: Color(0xFF1C1B1F),
-  surfaceVariant: Color(0xFFE7E0EC),
-  onSurfaceVariant: Color(0xFF49454F),
-  inverseSurface: Color(0xFF313033),
-  onInverseSurface: Color(0xFFF4EFF4),
-  inversePrimary: Color(0xFFD0BCFF),
-  shadow: Color(0xFF000000),
-  surfaceTint: Color(0xFF6750A4),
-  outlineVariant: Color(0xFFCAC4D0),
-  scrim: Color(0xFF000000),
-);
-
-const darkColorScheme = ColorScheme(
-  brightness: Brightness.dark,
-  primary: Color(0xFFD0BCFF),
-  onPrimary: Color(0xFF381E72),
-  primaryContainer: Color(0xFF4F378B),
-  onPrimaryContainer: Color(0xFFEADDFF),
-  secondary: Color(0xFFCCC2DC),
-  onSecondary: Color(0xFF332D41),
-  secondaryContainer: Color(0xFF4A4458),
-  onSecondaryContainer: Color(0xFFE8DEF8),
-  tertiary: Color(0xFFEFB8C8),
-  onTertiary: Color(0xFF492532),
-  tertiaryContainer: Color(0xFF633B48),
-  onTertiaryContainer: Color(0xFFFFD8E4),
-  error: Color(0xFFF2B8B5),
-  onError: Color(0xFF601410),
-  errorContainer: Color(0xFF8C1D18),
-  onErrorContainer: Color(0xFFF9DEDC),
-  outline: Color(0xFF938F99),
-  background: Color(0xFF1C1B1F),
-  onBackground: Color(0xFFE6E1E5),
-  surface: Color(0xFF1C1B1F),
-  onSurface: Color(0xFFE6E1E5),
-  surfaceVariant: Color(0xFF49454F),
-  onSurfaceVariant: Color(0xFFCAC4D0),
-  inverseSurface: Color(0xFFE6E1E5),
-  onInverseSurface: Color(0xFF313033),
-  inversePrimary: Color(0xFF6750A4),
-  shadow: Color(0xFF000000),
-  surfaceTint: Color(0xFFD0BCFF),
-  outlineVariant: Color(0xFF49454F),
-  scrim: Color(0xFF000000),
+final textTheme = TextTheme(
+  titleLarge: GoogleFonts.roboto(
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  ),
+  titleMedium: GoogleFonts.roboto(
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+  ),
+  titleSmall: GoogleFonts.roboto(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  ),
+  bodyMedium: GoogleFonts.roboto(
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  ),
+  bodySmall: GoogleFonts.roboto(
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+  ),
+  labelMedium: GoogleFonts.roboto(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    letterSpacing: 0.8,
+  ),
+  headlineMedium: GoogleFonts.roboto(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  ),
+  headlineSmall: GoogleFonts.roboto(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  ),
+  labelLarge: GoogleFonts.roboto(
+    fontWeight: FontWeight.w700,
+    fontSize: 13,
+  ),
 );
