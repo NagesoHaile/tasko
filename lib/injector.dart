6659cb168ca7ec232d3fda333cb624c899f70f91
NagesoHaile <nageehail12@gmail.com>
2024-09-07 06:31:34 +0300

    initial dependency injection registration.

---
+++ lib/injector.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+import 'package:tasko/core/helper/localdb.dart';
+import 'package:tasko/features/settings/bloc/settings_bloc.dart';
+import 'package:tasko/features/settings/services/settings_service.dart';
+
+final sl = GetIt.instance;
+Future<void> initDependencies() async {
+  sl.registerSingletonAsync<LocalDB>(
+    () async {
+      LocalDB localDB = LocalDB();
+      await localDB.init();
+      return localDB;
+    },
+    signalsReady: true,
+  );
+  sl.registerSingleton<SettingsService>(SettingsService());
+  sl.registerFactory<SettingsBloc>(() => SettingsBloc(sl()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tasko/app.dart';
+import 'package:tasko/injector.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
   runApp(const App());
 }
