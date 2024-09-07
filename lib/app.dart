74c67e0505d3de5a3dc745678ef6e190a9ca01e4
NagesoHaile <nageehail12@gmail.com>
2024-09-07 06:32:50 +0300

    providing the bloc setup

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tasko/configs/theme/theme.dart';
+import 'package:tasko/features/settings/bloc/settings_bloc.dart';
+import 'package:tasko/features/settings/services/settings_service.dart';
+import 'package:tasko/injector.dart';
 import 'package:tasko/router.dart';
 
 class App extends StatelessWidget {
@@ -7,11 +11,36 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: Style.light,
-      darkTheme: Style.dark,
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<SettingsBloc>(),
+        ),
+        // BlocProvider(
+        //   create: (context) => SubjectBloc(),
+        // ),
+      ],
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, state) {
+          return MaterialApp.router(
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode: () {
+              switch (state.appTheme) {
+                case AppTheme.light:
+                  return ThemeMode.light;
+                case AppTheme.dark:
+                  return ThemeMode.dark;
+                case AppTheme.system:
+                default:
+                  return ThemeMode.system;
+              }
+            }(),
+            debugShowCheckedModeBanner: false,
+            routerConfig: router,
+          );
+        },
+      ),
     );
   }
 }
