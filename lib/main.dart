71dafb31fc8f55993dd16af7ff7066b63e4439eb
NagesoHaile <nageehail12@gmail.com>
2024-09-07 13:13:45 +0300

    notification init.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:tasko/app.dart';
+import 'package:tasko/features/notification/notification_service.dart';
 import 'package:tasko/injector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initDependencies();
+  await NotificationService.initialize();
   runApp(const App());
 }
